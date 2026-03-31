a4c8f0f2dd176412772723e8f63e8bb8d1f5a0ca
devendr2025a <devendr2025a@gmail.com>
2026-03-31 11:53:47 +0530

    Update main.jsx
    Add changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import App from './App';
 import './index.css';
-
+// change
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
