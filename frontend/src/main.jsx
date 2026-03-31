5943b91fb6d0a2a27ee7ed0d9301c9b7bb8111c8
devendr2025a <devendr2025a@gmail.com>
2026-03-31 11:55:24 +0530

    Update main.jsx
    change fix the all changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import App from './App';
 import './index.css';
-// change
+// change fix the all changes
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
