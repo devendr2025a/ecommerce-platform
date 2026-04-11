76cd1efa59df45a9e6a0b273426005d4f8ec3e17
Khushi40222 <khushimy2006@gmail.com>
2026-04-11 18:46:51 +0530

    corrected cors

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use(
     origin: function (origin, callback) {
       // allow requests with no origin (like mobile apps or curl requests)
       if (!origin) return callback(null, true);
-      
+
       if (allowedOrigins.indexOf(origin) !== -1 || process.env.NODE_ENV === "development") {
         callback(null, true);
       } else {
